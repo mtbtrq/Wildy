961bca00c0cf41d284543059696b209c83892990
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2022-05-27 04:06:48 -0700

    Changed clearing messages duration from every hour to every 5 hours.

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ db.prepare(`CREATE TABLE IF NOT EXISTS ${config["msgTableName"]} (
 setInterval(() => {
     db.prepare(`DELETE FROM ${config["msgTableName"]}`).run()
     console.log("Cleared all messages!")
-}, 3600000)
+}, 18000000)
 
 app.listen(port, () => {
     console.log(`I am listening to requests on port ${port}`);
