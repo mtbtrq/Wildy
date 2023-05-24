29f90633966693e2de6183a02e2d721f4b7f70f1
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2023-05-24 02:18:10 -0700

    Switched to React 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(<App tab="home"/>)
 
 serviceWorkerRegistration.register();
