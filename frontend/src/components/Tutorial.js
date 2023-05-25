4ce5b07ba4b935672961bdd0363a60e021fa50ad
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2023-05-25 00:24:43 -0700

    Update Tutorial.js

--- frontend/src/components/Tutorial.js
+++ frontend/src/components/Tutorial.js
@@ -5,7 +5,7 @@ const Tutorial = () => {
     return(
         <>
             <video controls autoPlay>
-                <source src="https://private.up.railway.app/i/gn2h2" type="video/ogg"/>
+                <source src="https://private.up.railway.app/i/tutorial" type="video/ogg"/>
             </video>
 
             <Link to="/"><p>Back To Home</p></Link>
