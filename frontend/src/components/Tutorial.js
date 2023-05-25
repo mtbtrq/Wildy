495e713d1f6ec534de41967e910f29ec413fe644
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2023-05-24 23:05:26 -0700

    changed size of video

--- frontend/src/components/Tutorial.js
+++ frontend/src/components/Tutorial.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Tutorial = () => {
     return(
         <>
-            <video controls autoPlay width={1820} height={1080}>
+            <video controls autoPlay>
                 <source src="https://private.up.railway.app/i/gn2h2" type="video/ogg"/>
             </video>
 
